b763f6e406530e3b1519813190da2fb3febe9dff
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-23 16:10:08 +0900

    [실습] React 컴포넌트 속성(props) 타입 지정
    - React.ComponentProps 타입 활용
    - HTML 또는 SVG 엘리먼트 타입(문자값) 지정
    - 사용자 정의 추가 속성 확장 또는 인터섹션 타입 확장

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,11 @@
 import './app.css';
 import Playground from '../playgrounds/01-chip-list.tsx';
+import ReactForm from '../playgrounds/02-react-form.tsx';
 
 function App() {
   return (
     <div className="App">
-      <Playground />
+      <ReactForm />
     </div>
   );
 }
