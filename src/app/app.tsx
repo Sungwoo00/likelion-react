8b9b72dc7dfd3e8ff83a280c78c0e42046f4d9ac
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-24 16:13:59 +0900

    [실습] FormData 구하기 (onSubmit)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,12 @@
 import './app.css';
 import Playground from '../playgrounds/01-chip-list.tsx';
 import ReactForm from '../playgrounds/02-react-form.tsx';
+import SignUpForm from '@/playgrounds/03-signup-form.tsx';
 
 function App() {
   return (
     <div className="App">
-      <ReactForm />
+      <SignUpForm />
     </div>
   );
 }
--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -1,10 +1,11 @@
+import SignUpForm from '@/playgrounds/03-signup-form';
 import ReactForm from '../playgrounds/02-react-form';
 
 function Playground() {
   return (
     <div className="Playground">
       <h1>플레이그라운드</h1>
-      <ReactForm />
+      <SignUpForm />
     </div>
   );
 }
