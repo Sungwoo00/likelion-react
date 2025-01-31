ebcaea7b180c6470ba92c27c179386b9f565e0fc
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-31 16:32:09 +0900

    [실습] 로그인 기능 구현

--- src/homework/pages/sign-up.tsx
+++ src/homework/pages/sign-up.tsx
@@ -1,18 +1,55 @@
-import S from './sign-up.module.css';
-import FormInput from '../components/form-input';
 import ActionButton from '../components/action-button';
+import FormInput from '../components/form-input';
+import S from './sign-in.module.scss';
+import { useState } from 'react';
 
-function HomeworkSignUpForm() {
+interface SignInForm {
+  useremail: string;
+  userpassword: string;
+}
+
+function HomeworkSignIn() {
+  const [formData, setFormData] = useState<SignInForm>({
+    useremail: '',
+    userpassword: '',
+  });
+
+  const isAllInputted =
+    formData.useremail.length > 0 && formData.userpassword.length > 0;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    const nextFormData = {
+      ...formData,
+      [name]: value,
+    };
+
+    setFormData(nextFormData);
+  };
+
+  const handleSignIn = async (formData: FormData) => {
+    if (!isAllInputted) return;
+
+    const response = await fetch('http://localhost:4000/api/signin', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <section>
-      <h3 className="sr-only">회원가입 폼</h3>
-      <form className={S.signUpForm}>
-        <FormInput label="이름" name="username" placeholder="2글자 이상" />
+      <h3 className="sr-only">로그인 폼</h3>
+      <form className={S.signInForm} action={handleSignIn}>
         <FormInput
           type="email"
           label="이메일"
-          name="usermail"
+          name="useremail"
           placeholder="user@company.io"
+          value={formData.useremail}
+          onChange={handleChange}
         />
         <FormInput
           type="password"
@@ -20,18 +57,13 @@ function HomeworkSignUpForm() {
           name="userpassword"
           placeholder="숫자, 영문 조합 6자리 이상 입력"
           hasToggleButton
+          value={formData.userpassword}
+          onChange={handleChange}
         />
-        <FormInput
-          type="password"
-          label="패스워드 확인"
-          name="userpasswordConfirm"
-          placeholder="입력한 패스워드 다시 입력"
-          hasToggleButton
-        />
-        <ActionButton>회원가입</ActionButton>
+        <ActionButton aria-disabled={!isAllInputted}>로그인</ActionButton>
       </form>
     </section>
   );
 }
 
-export default HomeworkSignUpForm;
+export default HomeworkSignIn;
