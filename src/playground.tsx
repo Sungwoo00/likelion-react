69d16b4cd7084bf150b19d979c82ba058690de3a
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-02-03 22:29:52 +0900

    [실습] clsx 유틸리티 함수 활용
    - clsx/lite 모듈 활용
    - UI 축 변경 되지 않도록 설정 (overflow-y-scroll)

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import Nav from './homework/components/nav';
-import HomeworkSignIn from './homework/pages/sign-in';
+import HomeworkSignInForm from '@/homework/pages/sign-in';
 import HomeworkSignUpForm from './homework/pages/sign-up';
 import { getUIView, type UIView } from './components/lib/ui-views';
 
 function Playground() {
   const [uiView] = useState<UIView>(getUIView);
-  const isSignInView = uiView.includes('signin');
+  // 로그인 뷰(화면)인가요? 네(yes, true) or 아니오(no, false)
+  // boolean type
+  // const isSignInView = uiView.includes('signin');
+  const isSignInView = uiView === 'signin';
 
   return (
     <div className="Playground bg-euid-gray-200 wrapper">
       <h1>플레이그라운드</h1>
       <Nav />
-      {isSignInView ? <HomeworkSignIn /> : <HomeworkSignUpForm />}
+      {isSignInView ? <HomeworkSignInForm /> : <HomeworkSignUpForm />}
     </div>
   );
 }
