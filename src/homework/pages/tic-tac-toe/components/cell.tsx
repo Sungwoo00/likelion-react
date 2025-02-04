30a2af3c20a836693fd6cb3bd7521aee2552abb0
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-02-04 16:50:48 +0900

    [실습] 틱택토 게임 구현 part 2.
    - Grid 컴포넌트 상태
    - Grid 컴포넌트 파생된 상태
    - Grid 컴포넌트 상태 업데이트 함수

--- src/homework/pages/tic-tac-toe/components/cell.tsx
+++ src/homework/pages/tic-tac-toe/components/cell.tsx
@@ -1,13 +1,32 @@
-import { tm } from "@/utils/tw-merge";
 
-function Cell() {
+import { tm } from '@/utils/tw-merge';
+
+type CellProps = Omit<React.ComponentProps<'button'>, 'onClick'> & {
+  onPlay: () => void;
+};
+
+function Cell({ children, className = '', onPlay, ...restProps }: CellProps) {
+  const handlePlay = () => {
+    onPlay?.();
+  };
+
   return (
-    <button type="button" className={
-      tm('size-12 border rounded-md')
-    }>
-      셀
+    <button
+      type="button"
+      className={tm(
+        'cursor-pointer',
+        'size-16 border rounded-md',
+        'text-2xl font-semibold',
+        'border-black/50',
+        'hover:border-black hover:bg-slate-100/60',
+        className
+      )}
+      onClick={handlePlay}
+      {...restProps}
+    >
+      {children}
     </button>
-  )
+  );
 }
 
 export default Cell;
