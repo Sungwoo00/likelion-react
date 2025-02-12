94287e5cddfabd7dc9bf6e50905361aa5d0411b9
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-02-12 17:28:33 +0900

    [실습] DOM 접근/조작 2
    - useRef 훅 활용
    - 초깃값은 null
    - 요소 타입을 제네릭으로 설정
    - 컴포넌트 DOM 참조
    - 이펙트를 사용해 DOM 조작

--- src/pages/access-dom/index.tsx
+++ src/pages/access-dom/index.tsx
@@ -1,4 +1,4 @@
-import { createElement, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function AccessDOMPage() {
   // 컴포넌트 바디(body)
@@ -7,71 +7,89 @@ function AccessDOMPage() {
   // 상태 선언, 업데이트
   // 리액트 자동 화면 변경
 
+  // useRef 훅 함수 DOM 노드 접근 참조 객체 생성
+  const abbrRef = useRef<HTMLElement>(null); // { current: null }
+  console.log(0, { 'abbrRef.current': abbrRef.current });
+
+  // 이펙트
+  useEffect(() => {
+    // 컴포넌트 DOM 노드 접근/조작
+    if (abbrRef.current) {
+      console.log(1, { 'abbrRef.current': abbrRef.current });
+    }
+  }, []);
+
   const [isParse, setIsParse] = useState(false);
 
   // 사이드 이펙트 처리
-  // 리액트돔의 노드가 아닌, 실제 DOM 노드에 접근
-  // - 이벤트 핸들러
-  // - 이펙트 함수
-  // - ref 콜백 함수
+  // 리액트 돔의 노드가 아닌, 실제 DOM 노드에 접근
+  // - 이벤트 핸들러 (No 1.)
+  // - 이펙트 함수 (No 2.)
+  // - ref 콜백 함수 (No 3.)
 
-  // JSX -> React API (JavaScript) 코드 컴파일 -> 브라우저 해석(parsing) -> UI 렌더링 (브라우저 페인팅) => 사용자
-  return createElement(
-    'section',
-    {
-      ref: (element) => {
-        console.log('section', { element });
-      },
-    },
-    createElement(
-      'h2',
-      {
-        ref: (element) => {
-          console.log('section > h2', { element });
-        },
-        className: 'text-2xl text-react font-medium',
-      },
-      createElement(
-        'abbr',
-        {
-          ref: (element) => {
-            console.log('section > h2 > abbr', { element });
-          },
-          title: 'Document Object Model',
-          className: 'cursor-help no-underline',
-        },
-        isParse ? 'Document Object Model' : 'DOM'
-      ),
-      ' ',
-      '접근/조작'
-    ),
-    createElement(
-      'button',
-      {
-        type: 'button',
-        className: 'cursor-pointer px-2 py-0.5 bg-black text-white',
-        onClick: () => {
+  // const sectionRefCallback = () => {
+  //   // 사이드 이펙트 처리
+  //   const clearId = setInterval(() => console.log(Date.now()), 1000 / 60);
+
+  //   return () => {
+  //     clearInterval(clearId);
+  //     console.log('정리');
+  //   };
+  // };
+
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (searchInputRef.current) {
+        searchInputRef.current.focus();
+      }
+    }, 1000);
+  }, []);
+
+  // 마크업(markup) 생성
+  return (
+    <section
+    // ref={sectionRefCallback}
+    >
+      <h2 className="text-2xl text-react font-medium">
+        <abbr
+          ref={abbrRef}
+          title="Document Object Model"
+          className="cursor-help no-underline"
+        >
+          {isParse ? 'Document Object Model' : 'DOM'}
+        </abbr>{' '}
+        접근/조작
+      </h2>
+      <button
+        type="button"
+        onClick={() => {
           setIsParse((p) => !p);
-        },
-      },
-      'toggle button'
-    )
+        }}
+      >
+        DOM 용어 풀이
+      </button>
+
+      <form className="my-10">
+        <div>
+          <label htmlFor="like-a-book" className="sr-only">
+            선호 도서
+          </label>
+          <input
+            ref={searchInputRef}
+            id="like-a-book"
+            type="search"
+            placeholder="좋아하는 도서는?"
+            className="bg-white text-react px-3 py-1.5"
+          />
+        </div>
+        <button type="submit" className="bg-cyan-700 text-white py-2 w-20">
+          저장
+        </button>
+      </form>
+    </section>
   );
 }
 
 export default AccessDOMPage;
-
-// 마크업(markup) 생성
-// return (
-//   <section>
-//     <h2 className="text-2xl text-react font-medium">
-//       <abbr
-//         title="Document Object Model"
-//         className="cursor-help no-underline"
-//       >
-//         DOM
-//       </abbr>{' '}
-//       접근/조작
-//     </h2>
-//   </section>
-// )
